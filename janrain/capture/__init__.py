db40e065af9c3d81767f5f810e12b1307e64cacc
Micah Carrick <micah@janrain.com>
2013-03-20 16:39:44 -0700

    Version bump

--- janrain/capture/__init__.py
+++ janrain/capture/__init__.py
@@ -1,7 +1,7 @@
 from janrain.capture.api import Api
 from janrain.capture.exceptions import *
 
-VERSION = (0, 1, 2)
+VERSION = (0, 1, 3)
 
 def get_version():
     return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
