d92909b45694fe614b944fdfd5b4de7363e4a9a9
Colin von Heuring <colin@janrain.com>
2015-02-07 16:53:13 -0800

    [config] break out test of cluster merging

--- janrain/capture/test/test_config.py
+++ janrain/capture/test/test_config.py
@@ -17,10 +17,10 @@ class TestConfig(unittest.TestCase):
         cluster = config.get_cluster("dev")
         self.assertIn('client_id', cluster)
 
-        # get_client() should merge the cluster if it is referenced instead of
-        # an explicit client_id and client_secret
+    def test_merging(self):
+        # test merging clusters into clients
         client = config.get_client("cluster-client")
-        self.assertEqual(client['client_id'], cluster['client_id'])
+        self.assertEqual(client['client_id'], 'dev client_id')
 
     def test_defaults(self):
         # test convenience funcitons for getting defaults
