f9646ec88db693860aefe48e434127fb6bf14fca
Micah Carrick <micah@janrain.com>
2016-03-24 10:07:05 -0700

    TOOL-507 Break out version into a module so it can be used within package without circular dependency

--- janrain/capture/__init__.py
+++ janrain/capture/__init__.py
@@ -1,9 +1,3 @@
 from janrain.capture.api import Api
 from janrain.capture.exceptions import *
-
-VERSION = (0, 3, 7)
-
-def get_version():
-    return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
-
-__version__ = get_version()
+from janrain.capture.version import *
